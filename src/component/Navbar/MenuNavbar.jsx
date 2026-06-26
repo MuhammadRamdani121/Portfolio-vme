85db8584063a2261647019eaa13e742e769d2fca
MuhammadRamdani121 <muhamadramdani121.mr60@gmail.com>
2026-06-26 23:52:15 +0700

    Memperbarui MenuNavbar

--- src/component/Navbar/MenuNavbar.jsx
+++ src/component/Navbar/MenuNavbar.jsx
@@ -3,18 +3,22 @@ import NavbarItems from "./NavbarItems";
 export default function MenuNavbar() {
   return (
     <div>
-      <nav>
-        <NavbarItems to={"/about"} nama="About" />
+      <header className="flex items-center justify-between bg-red-500">
+        <img src="" alt="asd" />
 
-        <div>
-          <button>About</button>
+        <nav className="flex items-center justify-between gap-6">
+          <NavbarItems to={"/about"} nama="About" />
 
-          <div>
-            <NavbarItems to={"/about"} nama="A" />
-            <NavbarItems to={"/about"} nama="A" />
+          <div className="realtive group">
+            <button className="px-3 py-2">About</button>
+
+            <div className="absolute mt-2 hidden rounded bg-white shadow-md group-hover:block">
+              <NavbarItems to={"/about"} nama="A" />
+              <NavbarItems to={"/about"} nama="A" />
+            </div>
           </div>
-        </div>
-      </nav>
+        </nav>
+      </header>
     </div>
   );
 }
