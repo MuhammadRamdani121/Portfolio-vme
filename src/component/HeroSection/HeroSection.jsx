4299a32645d614ce24f46e893df5ac78391474e6
MuhammadRamdani121 <muhamadramdani121.mr60@gmail.com>
2026-06-27 20:42:26 +0700

    Memperbarui Hero Section

--- src/component/HeroSection/HeroSection.jsx
+++ src/component/HeroSection/HeroSection.jsx
@@ -1,10 +1,16 @@
 export default function HeroSection() {
   return (
     <div>
-      <main>
-        <section></section>
-        <aside>
-          <img src="/GambarDiri.jpg" alt="" />
+      <main className="grid h-screen grid-cols-1 items-center justify-center">
+        <section className="order-2 grid items-center justify-center">
+          <h1>Muhammad Ramdani</h1>
+        </section>
+        <aside className="order-1 grid items-center justify-center">
+          <img
+            src="/GambarDiri.jpg"
+            className="h-50 w-50 rounded-full"
+            alt=""
+          />
         </aside>
       </main>
     </div>
