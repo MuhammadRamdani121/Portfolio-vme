372e52c5f04919148f986317644da2ddb247adaf
MuhammadRamdani121 <muhamadramdani121.mr60@gmail.com>
2026-06-28 21:53:25 +0700

    Menambahkan Isi Data Navigation

--- src/data/Navigation.jsx
+++ src/data/Navigation.jsx
@@ -1,1 +1,23 @@
-export const Navigation = [];
+export const Navigation = [
+  {
+    nama: "Home",
+    path: "/",
+  },
+  {
+    nama: "About",
+    children: [
+      {
+        nama: "Nama 1",
+        path: "/nama 1",
+      },
+      {
+        nama: "Nama 2",
+        path: "/Nama 2",
+      },
+    ],
+  },
+  {
+    nama: "Footer",
+    path: "/Footer2",
+  },
+];
