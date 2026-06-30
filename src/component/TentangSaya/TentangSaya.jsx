e177782577810c297e72fa0338f55d6e8d2b1099
MuhammadRamdani121 <muhamadramdani121.mr60@gmail.com>
2026-06-30 21:02:47 +0700

    Memperbarui Tentang Saya

--- src/component/TentangSaya/TentangSaya.jsx
+++ src/component/TentangSaya/TentangSaya.jsx
@@ -5,13 +5,13 @@ export default function TentangSaya() {
         Tentang Saya
       </h1>
       <section>
-        <h1>
+        <h1 className="px-4 text-justify indent-1.5">
           Saya Muhammad Ramdani, seorang Front-End Web Developer yang memiliki
           minat dalam pengembangan website modern menggunakan React dan Tailwind
           CSS. Saya senang mempelajari teknologi baru dan membangun aplikasi
           yang bermanfaat.
         </h1>
-        <nav className="flex flex-col">
+        <nav className="flex justify-center gap-2">
           <a href="">Pendidikan</a>
           <a href="">Pengalaman</a>
           <a href="">Skills</a>
