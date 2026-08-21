a2b4b8c7b0ac7feaaa0289e0515b5b54af4f784f
MuhammadRamdani121 <muhamadramdani121.mr60@gmail.com>
2026-08-21 14:53:42 +0700

    Memperbarui APP Routing

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 
-import Home from "../pages/home";
+import Home from "../pages/Home";
 
 import PortfolioLayout from "../pages/Portfolio/PortfolioLayout";
 import ProjectPage from "../pages/Portfolio/ProjectPage";
